e3da04146769b7f96111fdae8a63421af95e50fd
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-03-25 10:38:20 +0000

    Create card_class.js

---
+++ card_class.js
@@ -0,0 +1,7 @@
+class card {                                        //Card Blueprint
+    constructor(id,value,suit){
+        this.id = id;                               //Each card has an integer id counting from 0 assigned to it when first created
+        this.value = value;                         //Each card has a value. This is an integer, and thus can be interpreted with 'if's to make a text value
+        this.suit = suit;                           //Each card has a suit. This is also an int, and functions identicallly to value.
+    }
+}
