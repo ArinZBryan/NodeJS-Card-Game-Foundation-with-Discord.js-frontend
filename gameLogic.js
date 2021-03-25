bf66fb86559dea2b77a2e7c31188a1e007307413
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-03-25 10:51:04 +0000

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,2 +1,5 @@
-let game = require(./game_class.js)
-let game = new game
+const game = require(./components/game_class.js)
+const card = require(./components/card_class.js)
+const pile = require(./components/pile_class.js)
+const player = require(./components/pile_class.js)
+let game = new game()
