f1688876e06ba37622c07e1c418ca90c61b29455
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-03-25 10:41:11 +0000

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,25 +1,5 @@
 
-class player {                                      //Blueprint for a player
-    constructor(cards,id){
-        this.cards = cards
-        this.id = id
-    }               //By defualt, the only variable that the player has is a list of cards, functioning as their deck. However, more can be added
-    sortDeck(method){                         //This functon of the player class can sort the cards of the player in several methods
-        switch (method) {
-            case "pure-numerical-ascending":        //This method groups the values of cards together in ascending order and can produce odd patterns of suits (uses bubble sort)
-                var sorted = []
-                for (let index = 0; index < this.cards.length; index++) {sorted.push([this.cards[index].value,this.cards[index].id])} //reduces the values held in each card to an array, [value,id]
-                    var len = sorted.length;                                                                           //length of hand
-                    for (var i = len-1; i>=0; i--){  
-                      for(var j = 1; j<=i; j++){
-                        if(sorted[j-1][0]>sorted[j][0]){
-                            var temp = sorted[j-1][0];
-                            sorted[j-1][0] = sorted[j][0];
-                            sorted[j][0] = temp;
-                        }
-                    }
-                }
-                for (let i = 0; i < this.cards.length; i++) {            //Takes values in 'sorted'  //for each card
+
 
             case "ascending-when-in-suits":     //This method groups togther suits and then orders the cards by value of suit within in ascending order
                 var sorted = []
