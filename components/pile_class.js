1abe0e1bea19e513c3420e42faa52bfd3f89ae2f
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-03-28 14:31:26 +0100

    .

--- components/pile_class.js
+++ components/pile_class.js
@@ -1,8 +1,13 @@
 class pile {
-	constructor(cards,id,owner){
+	constructor(cards,id,owner,xPos,yPos){
 		this.cards = cards;
 		this.id = id;
 		this.owner = owner;
+		this.display_properties = {
+			xPos : xPos,
+			yPos : yPos,
+			faceUp : faceUp
+		}
 	}
 }
 module.exports = {pile : pile};
--- gameLogic.js
+++ gameLogic.js
@@ -3,4 +3,9 @@ const card = require("./components/card_class.js");
 const pile = require("./components/pile_class.js");
 const player = require("./components/pile_class.js");
 let game = new game();
-
+module.exports = {
+    game:game,
+    card:card,
+    pile:pile,
+    player:player
+};
