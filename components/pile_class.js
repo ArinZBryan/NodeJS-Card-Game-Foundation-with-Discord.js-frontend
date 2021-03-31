9d7c390854f8bc42495f867533e0b8944688d7fd
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-03-31 21:26:54 +0100

    .

--- components/pile_class.js
+++ components/pile_class.js
@@ -7,8 +7,9 @@ class pile {
 			xPos : xPos,
 			yPos : yPos,
 			faceUp : faceUp,
-			renderStyle : renderStyle		//0 = directly on top of eachother //any positive number creates an offset downwards, any negative creates an offset upwards, these are max offset values, so,
-		}									// as cards build up to the max offset, the offset for each card will be half its height
+			renderStyle : renderStyle,		//0 = directly on top of eachother //any positive number creates an offset downwards, any negative creates an offset upwards, these are max offset values, so,
+			renderOffset: renderOffset									// as cards build up to the max offset, the offset for each card will be half its height
+		}
 	}
 }
 module.exports = {pile : pile};
